7e6d6ac21408d4623e420900daa933e2834f651b
Jackie <jacgut19@gmail.com>
2019-03-03 00:47:17 +0000

    finding one record with ID of char using toString

--- test/findtest.js
+++ test/findtest.js
@@ -3,10 +3,11 @@ const MarioChar = require('../models/mariochar');
 
 // Describe our tests
 describe('Finding records', function(){
-
+  var char;
+  
   // Add a character to the db before each tests
   beforeEach(function(done){
-    var char = new MarioChar({
+    char = new MarioChar({
       name: 'Mario'
     });
     char.save().then(function(){
@@ -21,15 +22,35 @@ describe('Finding records', function(){
     // /then(function(result that is finds, what is comes back with the record))
     MarioChar.findOne({name: 'Mario'}).then(function(result){
       
-      // found the result, found the record
-      // (represent the object that it returns to us which has a property of name)
+      // found the result/found the record(represent the object that it returns to us which has a property of name)
       assert(result.name === 'Mario'); 
       // if this result has a name property of Mario he this ahd successfully gone out 
       // and found one record with the name mario
 
       done();
     });
-
+  });
+  
+    // creating a new block
+    it('Finds a record by unique id', function(done){
+        
+        // finding one record with ID of char
+        MarioChar.findOne({_id: char._id}).then(function(result){
+            
+            // assert that the ID found is equal to the ID
+            // assert(result._id === char._id);
+            // this failed because the ids are objects not string
+            // this comparing two object, one is a local object and the other comes from the db
+            
+            // when using assert need to call toString on the id to get the actual string of the object
+            assert(result._id.toString() === char._id.toString());
+        
+        done();
+    });
   });
 
 });
+
+// **NOTE:**
+// When finding a character, don't have to put toString because mongoose 
+// understands that we want the to find a record with the same ID 
