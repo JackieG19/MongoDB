ecfb79c20592625dc78b0aa9fed6e48880931d39
Jackie <jacgut19@gmail.com>
2019-03-04 16:04:51 +0000

    adding another book into the database collection using the push method

--- test/nestTest.js
+++ test/nestTest.js
@@ -19,10 +19,16 @@ describe('Nesting records', function(){
             name: 'Patrick Rothfuss',
             books: [{title: 'Name of the Wind', pages: 400}]
         });
-
+        
+        // saving to the db
         pat.save().then(function(){
+            
+            // goes out and find the author and makes sure its save to the db
             Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+                
+                // the record that we found is equal to 1 of that array
                 assert(record.books.length === 1);
+                
                 done();
             });
         });
@@ -35,14 +41,25 @@ describe('Nesting records', function(){
             name: 'Patrick Rothfuss',
             books: [{title: 'Name of the Wind', pages: 400}]
         });
-
+        
+        // save to the db
         pat.save().then(function(){
             Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+                
                 // add a book to the books collection
                 record.books.push({title: "Wise Man's Fear", pages: 500});
+                // the book array.pushing a new book on to the array({passing in a new property})
+                
+                // refered recored returned to us.gonna save this record back into the db with the new book added in
                 record.save().then(function(){
+                    
+                    // when this book has successfully been saved...
+                    // go out and find the book again so it returns and makes sure it has the book
                     Author.findOne({name: 'Patrick Rothfuss'}).then(function(record){
+                        
+                        // makes sure this record has 2 books
                         assert(record.books.length === 2);
+                        
                         done();
                     });
                 });
